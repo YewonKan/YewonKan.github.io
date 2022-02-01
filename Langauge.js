0c329c5df571432178624fdfa110911e6516c9dd
Yewon <ga05019@naver.com>
2022-02-01 21:22:46 +0100

    css change

---
+++ Langauge.js
@@ -0,0 +1,8 @@
+// var data = {
+//     "english" :
+//     {
+//         "title" : "CV"
+//         "Description" :
+//             ""
+//     }
+// }
---
+++ function.js
@@ -0,0 +1,52 @@
+function myFunction(a) {
+    if (a == 1) {
+        var x = document.getElementById("myDIV1");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    else if(a==2){
+        var x = document.getElementById("myDIV2");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    else if(a==3){
+        var x = document.getElementById("myDIV3");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    else if(a==4){
+        var x = document.getElementById("myDIV4");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    else if(a==5){
+        var x = document.getElementById("myDIV5");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    else if(a==6){
+        var x = document.getElementById("myDIV6");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+
+}
